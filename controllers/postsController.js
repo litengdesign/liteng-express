8da0a7527f11804b67e50278079e76aa26178ac1
李腾 <827084369@qq.com>
2019-04-18 08:52:09 +0800

    .

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -101,7 +101,7 @@ const update = (request,response)=>{
     var whereArgs = {
         _id: _id
     };
-    Post.updateOne(whereArgs, body, { new: true })
+    Post.updateOne(whereArgs, body)
         .then(document => response.send(document))
 }
 
