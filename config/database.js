79ff753be27524d3bd97082303a21e8767937fd0
李腾 <827084369@qq.com>
2019-04-29 10:36:56 +0800

    修改数据库连接文件

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
-const uri = 'mongodb://www.hzscbg.com:27017/expressDB'
+const uri = 'mongodb://116.62.214.129:27017/expressDB'
 // const uri = 'mongodb://root:root@localhost:27017/expressDB'
-const options = {
-  useNewUrlParser: true,
-  authSource:"admin"
-}
+const options = {}
+// const options = {
+//   useNewUrlParser: true,
+//   authSource:"admin"
+// }
 mongoose.Promise = global.Promise
 mongoose
   .connect(uri, options)
