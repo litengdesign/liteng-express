6219d91546df8c7db03a82bb2714025016c9da0a
李腾 <827084369@qq.com>
2020-04-06 20:41:11 +0800

    修改数据库连接

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
-const uri = 'mongodb://116.62.214.129:27017/expressDB'
-const options = {}
+const uri = 'mongodb://root:Lee19910205@47.92.118.228:27017/mbAutomationDB'
+
 // const uri = 'mongodb://root:root@localhost:27017/expressDB'
-// const options = {
-//   useNewUrlParser: true,
-//   authSource:"admin"
-// }
+const options = {
+  useNewUrlParser: true,
+  authSource: "admin"
+}
 mongoose.Promise = global.Promise
 mongoose
   .connect(uri, options)
-  .then(db => console.log('😊数据库连接成功 ～'))
+  .then(db => console.log(uri + '😊数据库连接成功 ～'))
   .catch(error => console.log('数据库连接失败！'))
 
 module.exports = mongoose  
